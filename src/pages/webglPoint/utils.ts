c665f36d35407795690ea30f9c4776e01083ba6a
heyu <1395294694@qq.com>
2021-01-28 13:51:17 +0800

    feat: 给顶点着色器中的变量传值

--- src/pages/webglPoint/utils.ts
+++ src/pages/webglPoint/utils.ts
@@ -2,9 +2,11 @@ export default function drawPoint(canvas: HTMLCanvasElement) {
     const gl = canvas.getContext('webgl')!;
 
     const vertex = `
+    attribute vec2 a_position;
+    attribute float a_pointSize;
     void main() {
-        gl_Position     = vec4(.0, .0, .0, 1.0);
-        gl_PointSize     = 10.0;
+        gl_Position     = vec4( a_position, 0.0, 1.0 );
+        gl_PointSize     = a_pointSize;
     }
     `
     const fragment = `
@@ -29,6 +31,14 @@ export default function drawPoint(canvas: HTMLCanvasElement) {
     gl.linkProgram(program)
 
     gl.useProgram(program)
+
+    // 获取变量的地址
+    const a_position = gl.getAttribLocation(program, 'a_position')
+    // 往地址中传值
+    gl.vertexAttrib2f(a_position, -1.0 ,0.0 )
+
+    const a_pointSize = gl.getAttribLocation(program, 'a_pointSize')
+    gl.vertexAttrib1f(a_pointSize, 25.0)
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT)
